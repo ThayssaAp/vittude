e6d93ed514d476b6caca4cb0184077036af1f22f
Thayssa Apolinário <thayssaoliveira55@gmail.com>
2022-10-15 15:50:11 -0300

    created API Mock

---
+++ vittude-APIMock/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3010;
+const cors = require("cors")
+
+app.use(express.json());
+app.use(cors())
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body
+
+    if(email === "admin@admin.com" && password === "admin"){
+        return res.status(200).json({message: "Login efetuado com sucesso!"})
+    } 
+    return res.status(200).json({error: "E-mail ou senha incorreto!"})
+})
+
+app.listen(port, () => {
+    console.log("App listening on port ", port)
+});
